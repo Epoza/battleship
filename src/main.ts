d93a2679bba9063279624864992c1e0624b7452a
Epoza <86025026+Epoza@users.noreply.github.com>
2024-06-15 21:05:57 -0400

    Initialize the game

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,4 @@
-function analyzeArray(data: number[]) {
-  const average = () => {
-    const sum = data.reduce(
-      (accumulator: number, currentValue: number) => accumulator + currentValue,
-      0
-    );
-    return sum / data.length;
-  };
+import initializeGame from './initializeGame';
 
-  const min = () => {
-    return Math.min(...data);
-  };
-
-  const max = () => {
-    return Math.max(...data);
-  };
-
-  const length = () => {
-    return data.length;
-  };
-  return { average: average(), min: min(), max: max(), length: length() };
-}
-export default analyzeArray;
+// Initialize the game
+initializeGame();
